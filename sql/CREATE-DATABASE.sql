9afc94bb0adc4ff492d7a7e9442a5fc20cd9a463
Colin Maudry <colin@maudry.com>
2017-04-13 16:38:20 +0200

    Ajout d'un "name" au groupe #1

--- sql/CREATE-DATABASE.sql
+++ sql/CREATE-DATABASE.sql
@@ -30,6 +30,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `date_created` datetime DEFAULT NULL,
   `date_amended` datetime DEFAULT NULL,
   `date_deleted` datetime DEFAULT NULL,
