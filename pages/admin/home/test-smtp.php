905fd9215bc5dc07c2d722f0910ec3df90c09231
Clement <clement@garrot.org>
2017-04-29 17:32:47 +0200

    script d'envoi de mail pour tester facilement les différents paramètres #13

--- pages/admin/home/test-smtp.php
+++ pages/admin/home/test-smtp.php
@@ -1,2 +1,10 @@
 <?php
 
+global $_CONFIG;
+$_CONFIG["mail-isSMTP"]				        =true; //server smtp enable (use php mail function if false)
+$_CONFIG["mail-smtp-serveur"]		        ="XXXX"; //server smtp
+$_CONFIG["mail-smtp-login"]			        ="XXXX"; //server smtp login
+$_CONFIG["mail-smtp-pass"]			        ="XXXXX"; //server smtp pass
+$_CONFIG["mail-smtp-port"]			        =25; //port smtp
+Mail::sendMail("clement@k-mikaze.com", "AVEC SMTP", "OK HTML", "OK TXT", true);
+echo "ok";
