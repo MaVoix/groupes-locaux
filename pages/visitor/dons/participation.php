daebb1e236238e7c8417e15f049d8baa0f594f79
Clement <clement@garrot.org>
2017-04-29 20:07:26 +0200

    Formulaire de promesse de don

---
+++ pages/visitor/dons/participation.php
@@ -0,0 +1,18 @@
+<?php
+
+$bFoundGroup = false;
+$Group = new Group();
+if (isset($_GET["id"])) {
+    $Group = new Group(array("id" => intval($_GET["id"])));
+    $Group->hydrateFromBDD(array('*'));
+    if ($Group->getState() == "online") {
+        $bFoundGroup = true;
+    }
+}
+if (!$bFoundGroup) {
+    header("HTTP/1.0 404 Not Found");
+} else {
+    $aDataScript["group"] = $Group;
+    $aDataScript["subkey"] = sha1(substr($Group->getKey_edit(),0,10).$Group->getId());
+}
+
