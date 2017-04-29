a0f81e87b316879b46721e21eceacd9971ea5b01
Clement <clement@garrot.org>
2017-04-29 15:53:39 +0200

    Instancation du collectif sur la page (et mise en place de quelques éléments de base) #10

---
+++ pages/visitor/groupe/page.php
@@ -0,0 +1,16 @@
+<?php
+$bFoundGroup=false;
+$Group = new Group();
+if(isset($_GET["id"])){
+    $Group = new Group(array("id" =>intval($_GET["id"])));
+    $Group->hydrateFromBDD(array('*'));
+    if($Group->getState()=="online"){
+        $bFoundGroup=true;
+    }
+}
+
+if(!$bFoundGroup){
+    header("HTTP/1.0 404 Not Found");
+}else{
+    $aDataScript["group"]=$Group;
+}
