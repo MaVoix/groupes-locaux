d831a3ee39e50bc68df067aba500e573fe1f1ca9
Clement <clement@garrot.org>
2017-04-29 20:24:09 +0200

    SQL ajout de la table pledge

---
+++ sql/1.1.0-to1.x.x.sql
@@ -0,0 +1,40 @@
+--
+-- Structure de la table `pledge`
+--
+
+CREATE TABLE `pledge` (
+  `id` int(11) NOT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_amended` datetime DEFAULT NULL,
+  `date_deleted` datetime DEFAULT NULL,
+  `date_completed` datetime DEFAULT NULL,
+  `civilite` varchar(4) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `key_edit` int(11) DEFAULT NULL,
+  `group_id` int(11) NOT NULL,
+  `reference` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `pledge`
+--
+ALTER TABLE `pledge`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `pledge`
+--
+ALTER TABLE `pledge`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
--- sql/CREATE-DATABASE.sql
+++ sql/CREATE-DATABASE.sql
@@ -864,7 +864,51 @@ ALTER TABLE `group`
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+
+--
+-- Structure de la table `pledge`
+--
+
+CREATE TABLE `pledge` (
+  `id` int(11) NOT NULL,
+  `date_created` datetime DEFAULT NULL,
+  `date_amended` datetime DEFAULT NULL,
+  `date_deleted` datetime DEFAULT NULL,
+  `date_completed` datetime DEFAULT NULL,
+  `civilite` varchar(4) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `tel` varchar(20) DEFAULT NULL,
+  `amount` double DEFAULT NULL,
+  `key_edit` int(11) DEFAULT NULL,
+  `group_id` int(11) NOT NULL,
+  `reference` varchar(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `pledge`
+--
+ALTER TABLE `pledge`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `pledge`
+--
+ALTER TABLE `pledge`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
