06511f3e04535093c374b35bd870add5890aa473
Clement <clement@garrot.org>
2017-05-01 13:16:13 +0200

    Page de suivi des promesses

--- pages/mandataire/dons/suivi-promesse.php
+++ pages/mandataire/dons/suivi-promesse.php
@@ -7,6 +7,8 @@ $aDataScript["pledges"]=$PledgeList->getPage();
 
 
 //liste des transaction
-$TransactionList= new TransactionListe();
+/*
+ $TransactionList= new TransactionListe();
 $TransactionList->applyRules4Group($oMe->getGroup_id());
 $aDataScript["transaction"]=$TransactionList->getPage();
+*/
