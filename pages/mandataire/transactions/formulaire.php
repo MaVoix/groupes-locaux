e13cf41e90a3e54a6a5b8d5baa9d3933542ffed4
Clement <clement@garrot.org>
2017-05-01 17:59:18 +0200

    Validation d'une promesse par la création d'une transaction

--- pages/mandataire/transactions/formulaire.php
+++ pages/mandataire/transactions/formulaire.php
@@ -7,4 +7,20 @@ if(isset($_GET["id"])){
         $transaction=new Transaction();
     }
 }
+
+if(isset($_GET["from_pledge"])){
+
+    $pledge=new Pledge(array("id"=>intval($_GET["from_pledge"])));
+    $pledge->hydrateFromBDD(array("*"));
+
+    if( $pledge->getGroup_id()==$oMe->getGroup_id()){
+        $transaction=new Transaction();
+        $transaction->setAmount($pledge->getAmount());
+        $transaction->setPledge_id($pledge->getId());
+        $transaction->setReference($pledge->getReference());
+
+    }
+}
+
+
 $aDataScript["transaction"]= $transaction;
