9039b897d9c5ee964a662199f3764ec8cbd0bb3e
Clement <clement@garrot.org>
2017-05-01 16:09:41 +0200

    edition d'une transaction

--- pages/mandataire/transactions/formulaire.php
+++ pages/mandataire/transactions/formulaire.php
@@ -1,2 +1,10 @@
 <?php
-
+$transaction=new Transaction();
+if(isset($_GET["id"])){
+    $transaction=new Transaction(array("id"=>intval($_GET["id"])));
+    $transaction->hydrateFromBDD(array("*"));
+    if($transaction->getGroup_id()!=$oMe->getGroup_id()){
+        $transaction=new Transaction();
+    }
+}
+$aDataScript["transaction"]= $transaction;
