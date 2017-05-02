a80037bc280a480a5fcc79bc6dce2c1d7a82f1a9
Clement <clement@garrot.org>
2017-05-02 23:11:48 +0200

    ajustement twig de l'affichage des barres de progression #15

--- pages/visitor/dons/accueil.php
+++ pages/visitor/dons/accueil.php
@@ -15,6 +15,21 @@ foreach($groups as $datagroup){
     $datagroup["circonscription"]= $oCirconscription;
     $datagroup["mandataire"]=$group->getMandataire();
     $datagroup["path_pic_fit"]=$group->getPath_pic_fit();
+    //protection division par 0
+    if($group->getAmount_target()==0){
+        $group->setAmount_target(1);
+    }
+    $datagroup["pledge_amount"]=2500;
+    $datagroup["pledge_percent"]= round($datagroup["pledge_amount"]*100/ $group->getAmount_target());
+    $datagroup["income_amount"]=1250;
+    $datagroup["income_percent"] = round($datagroup["income_amount"]*100/ $group->getAmount_target());
+    if($datagroup["income_percent"]>100){
+        $datagroup["income_percent"]=100;
+        $datagroup["pledge_percent"]=0;
+    }
+    if($datagroup["pledge_percent"]+$datagroup["income_percent"]>100){
+        $datagroup["pledge_percent"]=100-$datagroup["income_percent"];
+    }
     $groupsOut[$oDepartement->getCode()."-".$oCirconscription->getCode()."-".$group->getId()]=$datagroup;
 
 }
