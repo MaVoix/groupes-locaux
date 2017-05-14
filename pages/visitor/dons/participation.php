e36401550df961791a3e4a6c1e89c7c1b0d86751
Colin Maudry <colin@maudry.com>
2017-05-14 21:01:00 +0200

    Affichage du budget détaillé de la circonscription au moment du don

--- pages/visitor/dons/participation.php
+++ pages/visitor/dons/participation.php
@@ -15,5 +15,30 @@ if (!$bFoundGroup) {
     $aDataScript["group"] = $Group;
     $aDataScript["amountMax"]= $Group->getAmount_target()-$Group->getAmount_plegde()-$Group->getAmount_income();
     $aDataScript["subkey"] = sha1(substr($Group->getKey_edit(),0,10).$Group->getId());
+    $aDataScript["postersCost"] = round($Group->getPosters() * 1.5554285714, 2);
+    $aDataScript["professions_de_foiCost"] = round($Group->getProfessions_de_foi() * 0.0106656343, 2);
+    $aDataScript["ballotsEtrCost"] = round($Group->getBallots() * 0.0028419347, 2);
+    $aDataScript["ballotsCost"] = round($Group->getBallots() * 0.002532821, 2);
+    $aDataScript["smallExpensesCost"] = 100;
+    $aDataScript["accountantCost"] = 150;
+    $aDataScript["bankingCost"] = 150;
+    $aDataScript["accountantEtrCost"] = 115;
+    $aDataScript["postalCost"] = 60;
+    $aDataScript["remainingCost"] =
+    $Group->getAmount_target()
+    - $aDataScript["postalCost"]
+    - $aDataScript["accountantCost"]
+    - $aDataScript["smallExpensesCost"]
+    - $aDataScript["postersCost"]
+    - $aDataScript["bankingCost"]
+    - $aDataScript["professions_de_foiCost"]
+    - $aDataScript["ballotsCost"];
+    $aDataScript["remainingEtrCost"] =
+    $Group->getAmount_target()
+    - $aDataScript["bankingCost"]
+    - $aDataScript["postalCost"]
+    - $aDataScript["accountantEtrCost"]
+    - $aDataScript["smallExpensesCost"]
+    - $aDataScript["postersCost"]
+    - $aDataScript["ballotsEtrCost"];
 }
-
