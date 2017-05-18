8f053e96abfd1af69a6cf67124bc154c9e1de38a
Colin Maudry <colin@maudry.com>
2017-05-18 19:23:07 +0200

    Intégration du prix de la livraison au prix unitaire des affiches

--- pages/visitor/groupe/page.php
+++ pages/visitor/groupe/page.php
@@ -13,7 +13,7 @@ if(!$bFoundGroup){
     header("HTTP/1.0 404 Not Found");
 }else{
     $aDataScript["group"]=$Group;
-    $aDataScript["postersCost"] = round($Group->getPosters() * 1.5554285714, 2);
+    $aDataScript["postersCost"] = round($Group->getPosters() * 1.8161, 2);
     $aDataScript["professions_de_foiCost"] = round($Group->getProfessions_de_foi() * 0.0106656343, 2);
     $aDataScript["ballotsEtrCost"] = round($Group->getBallots() * 0.0028419347, 2);
     $aDataScript["ballotsCost"] = round($Group->getBallots() * 0.002532821, 2);
