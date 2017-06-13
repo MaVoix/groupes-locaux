10a1526a54035c16aaa47e4f13d7ab3bba1823b3
Colin Maudry <colin@maudry.com>
2017-06-13 18:26:05 +0200

    Ajout d'un champ emailing_expenses, nouveaux objectectifs et quantités #52

---
+++ sql/2.11.0-2.12.0.sql
@@ -0,0 +1,35 @@
+ALTER TABLE `group` ADD `emailing_expenses` int(11) 0 AFTER `ballots`;
+
+
+UPDATE `group` SET posters='126',ballots='76150',professions_de_foi='79958',amount_target='2063' WHERE id='3';
+UPDATE `group` SET posters='302',ballots='224400',professions_de_foi='109636',amount_target='2954' WHERE id='4';
+UPDATE `group` SET posters='400',ballots='219998',professions_de_foi='109999',amount_target='3136' WHERE id='5';
+UPDATE `group` SET posters='105',ballots='150700',professions_de_foi='72728',amount_target='1943' WHERE id='6';
+UPDATE `group` SET posters='250',ballots='132950',professions_de_foi='63755',amount_target='2047' WHERE id='7';
+UPDATE `group` SET posters='140',ballots='161744',professions_de_foi='80872',amount_target='2147' WHERE id='8';
+UPDATE `group` SET posters='406',ballots='198967',professions_de_foi='99483',amount_target='2961' WHERE id='9';
+UPDATE `group` SET posters='100',ballots='155300',professions_de_foi='75292',amount_target='1979' WHERE id='10';
+UPDATE `group` SET posters='360',ballots='239205',professions_de_foi='119602',amount_target='3234' WHERE id='11';
+UPDATE `group` SET posters='300',ballots='197259',professions_de_foi='103014',amount_target='2826' WHERE id='12';
+UPDATE `group` SET posters='150',ballots='151500',professions_de_foi='72599',amount_target='2022' WHERE id='13';
+UPDATE `group` SET posters='200',ballots='166404',professions_de_foi='79420',amount_target='2255' WHERE id='14';
+UPDATE `group` SET posters='90',ballots='130971',professions_de_foi='65485',amount_target='1779' WHERE id='16';
+UPDATE `group` SET posters='300',ballots='168963',professions_de_foi='80602',amount_target='2487' WHERE id='17';
+UPDATE `group` SET posters='255',ballots='156004',professions_de_foi='89780',amount_target='2502' WHERE id='18';
+UPDATE `group` SET posters='150',ballots='173180',professions_de_foi='82842',amount_target='2204' WHERE id='19';
+UPDATE `group` SET posters='90',ballots='93883',professions_de_foi='93883',amount_target='2238' WHERE id='20';
+UPDATE `group` SET posters='330',ballots='211730',professions_de_foi='105865',amount_target='2936' WHERE id='21';
+UPDATE `group` SET posters='213',ballots='104573',professions_de_foi='76022',amount_target='2040' WHERE id='22';
+UPDATE `group` SET posters='90',ballots='163000',professions_de_foi='78750',amount_target='2036' WHERE id='23';
+UPDATE `group` SET posters='189',ballots='180201',professions_de_foi='86482',amount_target='2340' WHERE id='24';
+UPDATE `group` SET posters='183',ballots='109690',professions_de_foi='73176',amount_target='1978' WHERE id='25';
+UPDATE `group` SET posters='240',ballots='166500',professions_de_foi='80141',amount_target='2319' WHERE id='27';
+UPDATE `group` SET posters='160',ballots='193127',professions_de_foi='96563',amount_target='2463' WHERE id='28';
+UPDATE `group` SET posters='150',ballots='137421',professions_de_foi='73500',amount_target='2031' WHERE id='29';
+UPDATE `group` SET posters='135',ballots='157740',professions_de_foi='80000',amount_target='2110' WHERE id='30';
+UPDATE `group` SET posters='150',ballots='183500',professions_de_foi='89931',amount_target='2328' WHERE id='31';
+UPDATE `group` SET posters='118',ballots='138644',professions_de_foi='66171',amount_target='1866' WHERE id='43';
+UPDATE `group` SET posters='180',ballots='182870',professions_de_foi='88784',amount_target='2363' WHERE id='44';
+UPDATE `group` SET posters='150',ballots='130909',professions_de_foi='62479',amount_target='1858' WHERE id='45';
+UPDATE `group` SET posters='324',ballots='236000',professions_de_foi='115543',amount_target='3099' WHERE id='46';
+UPDATE `group` SET posters='200',ballots='173034',professions_de_foi='82564',amount_target='2304' WHERE id='47';
