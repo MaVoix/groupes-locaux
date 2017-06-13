e2d0fbb373313a2d7e3eb46fa3340d6dd94b6903
Colin Maudry <colin@maudry.com>
2017-06-14 00:20:20 +0200

    Actualisation de lignes de budgets #52

--- pages/visitor/groupe/page.php
+++ pages/visitor/groupe/page.php
@@ -12,33 +12,16 @@ if(isset($_GET["id"])){
 if(!$bFoundGroup){
     header("HTTP/1.0 404 Not Found");
 }else{
-    $aDataScript["pledge_amount"]=$Group->getAmount_plegde();
-    $aDataScript["income_amount"]=$Group->getAmount_income();
-    $aDataScript["group"]=$Group;
-    $aDataScript["postersCost"] = round($Group->getPosters() * 1.8161, 2);
-    $aDataScript["professions_de_foiCost"] = round($Group->getProfessions_de_foi() * 0.0106656343, 2);
-    $aDataScript["ballotsEtrCost"] = round($Group->getBallots() * 0.0028419347, 2);
-    $aDataScript["ballotsCost"] = round($Group->getBallots() * 0.002532821, 2);
-    $aDataScript["smallExpensesCost"] = 100;
-    $aDataScript["accountantCost"] = 150;
-    $aDataScript["bankingCost"] = 150;
-    $aDataScript["bankingEtrCost"] = 115;
-    $aDataScript["postalCost"] = 60;
-    $aDataScript["remainingCost"] =
-    $Group->getAmount_target_withExpenses()
-    - $aDataScript["postalCost"]
-    - $aDataScript["accountantCost"]
-    - $aDataScript["smallExpensesCost"]
-    - $aDataScript["postersCost"]
-    - $aDataScript["bankingCost"]
-    - $aDataScript["professions_de_foiCost"]
-    - $aDataScript["ballotsCost"];
-    $aDataScript["remainingEtrCost"] =
-    $Group->getAmount_target_withExpenses()
-    - $aDataScript["bankingEtrCost"]
-    - $aDataScript["postalCost"]
-    - $aDataScript["accountantCost"]
-    - $aDataScript["smallExpensesCost"]
-    - $aDataScript["postersCost"]
-    - $aDataScript["ballotsEtrCost"];
+  $aDataScript["pledge_amount"]=$Group->getAmount_plegde();
+  $aDataScript["income_amount"]=$Group->getAmount_income();
+  $aDataScript["group"]=$Group;
+  $aDataScript["postersCost"] = round($Group->getPosters() * 1.8161, 2);
+  $aDataScript["professions_de_foiCost"] = round($Group->getProfessions_de_foi() * 0.011613687, 2);
+  $aDataScript["ballotsEtrCost"] = round($Group->getBallots() * 0.0028419347, 2);
+  $aDataScript["ballotsCost"] = round($Group->getBallots() * 0.003056503, 2);
+  $aDataScript["accountantCost"] = 150;
+  $aDataScript["postExpenses"] = $Group->getPost_expenses();
+  $aDataScript["emailingExpenses"] = $Group->getEmailing_expenses();
+  $aDataScript["smallExpenses"] = $Group->getSmall_expenses();
+  $aDataScript["bankingFees"] = $Group->getBanking_fees();
 }
