01831fbc981a7bcb1d51bf5598bb5cf2b2135bba
Colin Maudry <colin@maudry.com>
2017-06-15 00:54:28 +0200

    Grâce aux nouveaux champs dans la table group, plus besoin d'instancier les données

--- pages/visitor/dons/participation.php
+++ pages/visitor/dons/participation.php
@@ -15,13 +15,4 @@ if (!$bFoundGroup) {
       $aDataScript["group"] = $Group;
       $aDataScript["amountMax"]= $Group->getAmount_target_withExpenses()-$Group->getAmount_pledge()-$Group->getAmount_income();
       $aDataScript["subkey"] = sha1(substr($Group->getKey_edit(),0,10).$Group->getId());
-      $aDataScript["postersCost"] = round($Group->getPosters() * 1.8161, 2);
-      $aDataScript["professions_de_foiCost"] = round($Group->getProfessions_de_foi() * 0.011613687, 2);
-      $aDataScript["ballotsEtrCost"] = round($Group->getBallots() * 0.0028419347, 2);
-      $aDataScript["ballotsCost"] = round($Group->getBallots() * 0.003056503, 2);
-      $aDataScript["accountantCost"] = 150;
-      $aDataScript["postExpenses"] = $Group->getPost_expenses();
-      $aDataScript["emailingExpenses"] = $Group->getEmailing_expenses();
-      $aDataScript["smallExpenses"] = $Group->getSmall_expenses();
-      $aDataScript["bankingFees"] = $Group->getBanking_fees();
   }
